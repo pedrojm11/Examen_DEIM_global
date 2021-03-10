11548cfdc397f9cc6017ba6ca518d61dd1f2627c
pedrojm11 <>
2021-03-10 20:44:58 +0100

    he consegido generar los huevos pero no que caigan

---
+++ Jimenez Mateos Pedro/Assets/scripts/Crearbolas.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Crearbolas : MonoBehaviour
+{
+    [SerializeField] GameObject Huevo;
+    [SerializeField] Transform RefPos;
+    [SerializeField] float distobstacle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("Huevoscorrutine");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CrearHuevo();
+    }
+    void CrearHuevo()
+    {
+        //Crear una variable para que genere un numero aleatorio entre los dos rangos que le asigno
+        float posRandom = Random.Range(0f, 30f);
+        //Creas un nuevo vector que va a ser el destino donde se genere la columna, el posramdom al principio debido a que queremos que se genere en el eje x en el centro seria el eje y , z en ultimo lugar
+        Vector3 DestPos = new Vector3(posRandom, posRandom, posRandom);
+        //Este vector coge la posicion de referencia y le añade ese vector aleatorio
+        Vector3 NewPos = RefPos.position + DestPos;
+        //Instancio el prefab en la posición del objeto de referencia
+        //Como tenemos su componente Transform, le indicamos que lo que quiero es su posición
+        Instantiate(Huevo, NewPos, Quaternion.identity);
+    }
+    IEnumerator HuevosCorrutine()
+    {
+        //for(puntodeinicio,condicion,accionfinal) este bucle nos sirve para crear la columna cada x tiempo
+        for (int n = 0; ; n++)
+        {
+            //para ver las columnas que estoy generando
+            print(n);
+            //Intancio el prefab en coordenadas 0,0,0
+            //Instantiate(MyColumn); esto solo es necesario si me pide que lo cree en 0,0,0
+            //aqui como queremos que se genere en la posicion ramdom asignamos el metodo que hemos creado para que haga esto
+            CrearHuevo();
+            //Dentro del for hacemos un yield return que basicamente es decirle al for que pare antes de volver hacerse
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
---
+++ Jimenez Mateos Pedro/Assets/scripts/Crearbolas2.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Crearbolas2 : MonoBehaviour
+{
+
+    [SerializeField] GameObject bolas;
+    [SerializeField] Transform InitPos;
+    [SerializeField] Text numGlobos;
+    public int cache;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InicioBolas();
+        StartCoroutine("bolasCorrutina");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        contarNaves();
+    }
+    void InicioBolas()
+    {
+
+        for (cache = 0; cache < 20; cache++)
+        {
+            float posRandomx = Random.Range(-40f, 40f);
+            float posRandomz = Random.Range(-40f, 40f);
+            float posRandomy = Random.Range(0f, 10f);
+            Vector3 NewPos = new Vector3(posRandomx, posRandomy, posRandomz);
+            Vector3 finalPos = InitPos.position + NewPos;
+            Instantiate(bolas, finalPos, Quaternion.identity);
+        }
+
+    }
+    void generarBolas()
+    {
+        float posRandomx = Random.Range(-40f, 40f);
+        float posRandomz = Random.Range(-40f, 40f);
+        float posRandomy = Random.Range(0f, 10f);
+        Vector3 NewPos = new Vector3(posRandomx, posRandomy, posRandomz);
+        Instantiate(bolas, NewPos, Quaternion.identity);
+    }
+    IEnumerator bolasCorrutina()
+    {
+        for (; cache <= 24; cache++)
+        {
+            generarBolas();
+            yield return new WaitForSeconds(4);
+        }
+        for (cache = 25; cache <= 30; cache++)
+        {
+            generarBolas();
+            yield return new WaitForSeconds(2);
+        }
+        for (cache = 31; ; cache++)
+        {
+
+            generarBolas();
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+    void contarNaves()
+    {
+        numGlobos.text = "Globos: " + cache;
+    }
+}
