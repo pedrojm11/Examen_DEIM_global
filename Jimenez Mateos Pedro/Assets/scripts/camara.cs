a17ab0c1efec360c205f05604f14a10e5dd81234
pedrojm11 <>
2021-03-10 19:57:16 +0100

    asociada camara a objeto

---
+++ Jimenez Mateos Pedro/Assets/scripts/camara.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camara : MonoBehaviour
+{
+    [SerializeField] Transform playerPosition;
+    [SerializeField] float smoothVelocity = 0.3F;
+    [SerializeField] Vector3 camaraVelocity = Vector3.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(playerPosition.position.x, playerPosition.position.y + 150, playerPosition.position.z - 150);
+    }
+}
